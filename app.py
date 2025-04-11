c3f93ae8bad44b3181f08e219ff97e2c82bcb7eb
Zuhaib_khan <77223355+zuhaib-cloud@users.noreply.github.com>
2025-04-11 15:55:07 -0400

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,20 @@
 from flask import Flask
+import socket
+
 app = Flask(__name__)
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
 
 @app.route('/')
 def hello_cloud():
-    return 'Welcome to khan final test API server'
+  return 'Hello Cloud!Welcome to khan final test API server'
+  
+@app.route('/host')
+def host_name():
+  return hostname
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0')
+@app.route('/ip')
+def host_ip():
+  return ip_address
+
+app.run(host='0.0.0.0')
