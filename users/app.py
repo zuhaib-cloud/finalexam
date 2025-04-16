b012c42b0fbb6101ca57deb0807497ed3f0fe237
Zuhaib_khan <77223355+zuhaib-cloud@users.noreply.github.com>
2025-04-16 01:16:25 -0400

    Create app.py

---
+++ users/app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+import socket
+ 
+app = Flask(__name__)
+hostname = socket.gethostname()
+ip_address = socket.gethostbyname(hostname)
+ 
+@app.route('/')
+def hello_cloud():
+  return 'User task'
+@app.route('/host')
+def host_name():
+  return hostname
+ 
+@app.route('/ip')
+def host_ip():
+  return ip_address
+ 
+app.run(host='0.0.0.0')
